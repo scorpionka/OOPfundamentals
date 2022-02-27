cf03e06c15b748967b2c2b5b6a30803df7495076
Aliaksei Kuzmitski <Aliaksei_Kuzmitski@epam.com>
2022-02-28 00:49:07 +0300

    Add project files.

---
+++ LibraryClient/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace LibraryClient
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
